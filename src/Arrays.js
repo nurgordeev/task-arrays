d396c64d00b5c0c10c64f8f7b86ddaaece95ad7b
Evgenii <applepienur@gmail.com>
2022-03-27 20:27:42 +0500

    Task arrays

--- src/Arrays.js
+++ src/Arrays.js
@@ -3,18 +3,30 @@
   Преобразуте строку в массив чисел, отсортируйте их по возрастанию и верните строку с упорядоченными числами,
   разделенными запятыми.
 */
-export function arraySorting(data) {}
-
+export function arraySorting(data) {
+	let arr = data.split(',');
+	arr.sort((a, b) => a - b);
+	return arr.toString();
+}
 /*
   В функцию arrayFiltering() приходит массив целых чисел.
   Преобразуйте массив таким образом, чтобы все элементы массива, в которых содержатся числа более 100,
   были бы удалены из массива, и верните его в качестве результата функции.
 */
-export function arrayFiltering(data) {}
+export function arrayFiltering(data) {
+	let result = data.filter(function (number) {
+		return number < 100;
+	});
+	return result;
+}
 
 /*
   В функцию arrayPushing() приходят два массива целых чисел размерностью 5 элементов.
   Получите третий массив размерностью 10 элементов, в котором последовательно чередуются
   значения из первых двух массивов, и верните его в качестве результата функции.
 */
-export function arrayPushing(array1, array2) {}
+export function arrayPushing(array1, array2) {
+	for (let i = 0; array1.length;)
+		array2.splice(i++ * 2, 0, array1.shift());
+	return array2;
+}
